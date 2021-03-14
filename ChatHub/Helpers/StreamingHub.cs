66b325d5a9ee031da6d00913ddeddf04c0ec57b4
Felipe Alves <felipealves0891@gmail.com>
2021-03-14 15:14:39 -0300

    first commit

---
+++ ChatHub/Helpers/StreamingHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatHub.Helpers
+{
+    public class StreamingHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
+        }
+    }
+}
---
+++ ChatHub/wwwroot/main.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/streaminghub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    if (user == null) user = "anonymous";
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var name = user.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var li = document.createElement("li");
+    li.textContent = msg + " - " + name;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    var li = document.createElement("li");
+    li.textContent = "Connetado!";
+    document.getElementById("messagesList").appendChild(li);
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.querySelector("#btnSend").addEventListener('click', function () {
+    const message = document.querySelector('#tMessage').value;
+    connection.invoke("SendMessage", message);
+    document.querySelector('#tMessage').value = "";
+}, false)
